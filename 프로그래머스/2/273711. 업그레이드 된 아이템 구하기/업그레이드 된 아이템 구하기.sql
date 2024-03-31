4d6e4814a393be59e967c0dede0271b6201401f8
JaHyeon_Gu <jahyungu@naver.com>
2024-03-31 19:37:55 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,16 @@
+-- 코드를 작성해주세요
+SELECT 
+    ITEM_ID,
+    ITEM_NAME,
+    RARITY
+FROM ITEM_INFO
+WHERE ITEM_ID IN (SELECT ITEM_ID
+                  FROM ITEM_TREE
+                  WHERE PARENT_ITEM_ID IN (SELECT ITEM_ID
+                                           FROM ITEM_INFO
+                                           WHERE RARITY = 'RARE')
+                  AND PARENT_ITEM_ID IS NOT NULL)
+ORDER BY ITEM_ID DESC
+
+
+
